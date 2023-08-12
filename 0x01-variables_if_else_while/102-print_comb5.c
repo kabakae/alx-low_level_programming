b4f4ef05be347be6109cfed65db345435cf2660e
kabakae <kabakaevans@gmail.com>
2023-08-12 00:31:28 -0700

    m

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -20,7 +20,8 @@ int main(void)
 					putchar(' ');
 					putchar(num2 / 10 + '0');
 					putchar(num2 % 10 + '0');
-					if (!(num1 == 99 && num2 == 99))
+					if (num1 == 99 && num2 == 99)
+					continue;
 					{
 						putchar(',');
 						putchar(' ');
