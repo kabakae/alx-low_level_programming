6a284ff8869d030a9d02fd5f57649f92489512e4
kabakae <kabakaevans@gmail.com>
2023-09-07 04:20:17 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -6,7 +6,7 @@
  * @s1: ....
  * @s2: ......
  * @n: ....
- *
+ * Return: ....
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
@@ -33,7 +33,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		if (j <= i)
 			str[j] = s1[j];
-		if (j = i)
+		if (j >= i)
 		{
 			str[j] = s2[k];
 			k++;
