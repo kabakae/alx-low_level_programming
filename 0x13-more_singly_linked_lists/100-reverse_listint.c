ecba3682ad282314ea02f0900e98b1b44d361c1a
kabakae <kabakaevans064@gmail.com>
2023-09-28 10:54:10 -0700

    100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -4,7 +4,7 @@
  * reverse_listint - function that reverses a listint_t linked list.
  * @head: pointer to listint_t
  *
- * Returns: a pointer to the first node of the reversed list
+ * Return: a pointer to the first node of the reversed list
  */
 listint_t *reverse_listint(listint_t **head)
 {
