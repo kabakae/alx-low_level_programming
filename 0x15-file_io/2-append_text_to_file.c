54b842b6ba3a1c8e637e703ba0a9bd5a77cfd799
kabakae <kabakaevans064@gmail.com>
2023-10-30 09:54:07 -0700

    Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,24 +8,27 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, res_write, len;
+	int fd;
+	ssize_t text_length = 0;
+	ssize_t write_result;
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC,  S_IWUSR);
+	if (text_content == NULL)
+		return (1);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
-		return (-1);
-	if (text_content)
+	       return (-1);
+
+	while (text_content[text_length])
+
+
+		text_length++;
+	write_result = write(fd, text_content,(size_t)text_length);
+	if (write_result == -1 || write_result != text_length)
 	{
-		len = 0;
-		while (text_content[len] != '\0')
-			len++;
-		res_write = write(fd, text_content, len);
-		if (res_write == -1 || res_write != len)
-		{
-			close(fd);
-			return (-1);
-		}
+		close(fd);
+		return (-1);
 	}
 	close(fd);
 	return (1);
