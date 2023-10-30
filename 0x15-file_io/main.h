8262df818915293f5496296c2404c578a7d2d21b
kabakae <kabakaevans064@gmail.com>
2023-10-30 13:30:37 -0700

    main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,12 +4,17 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+#define BUFFER_SIZE 1024
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
-
-
+int main(int argc, char *argv[]);
 
 
 
