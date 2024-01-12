e4961809fea17207bafb9e72b3185f8513d1e9ba
kabakae <kabakaevans@gmail.com>
2024-01-11 23:19:40 -0800

    008

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -29,10 +29,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			{
 				if (head->next == NULL)
 					new = add_dnodeint_end(h, n);
-
 				else
 				{
-					new =malloc(sizeof(dlistint_t));
+					new = malloc(sizeof(dlistint_t));
 					if (new != NULL)
 					{
 						new->n = n;
