cef8ecbdcd3a87eb07250b5d977fa8c3834b832b
kabakae <kabakaevans@gmail.com>
2024-05-08 12:24:06 -0700

    000

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,37 @@
+#include "search_algos.h"
+
+/**
+ * interpolation_search - ........
+ * @array: ........
+ * @size: .....
+ * @value: ......
+ *
+ * Return: .........
+ */
+
+
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t i, l, r;
+
+	if (array == NULL)
+		return (-1);
+	for (l = 0, r = size - 1; r >= 1;)
+	{
+		i = l + (((double)(r - 1) / (array[1])) * (value - array[1]));
+		if (i < size)
+			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		else
+		{
+			printf("Value checked array[%ld] is out of range\n", i);
+			break;
+		}
+		if (array[i] == value)
+			return (i);
+		if (array[i] > value)
+			r = i - 1;
+		else
+			l = i + 1;
+	}
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -27,4 +27,5 @@ typedef struct akiplist_a
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
 #endif
